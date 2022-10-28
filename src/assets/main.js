119085288c74a7d3a18454fd7bc5f1920296be66
Sofia Monico <sofiamonico02@gmail.com>
2022-10-28 11:04:48 -0300

    [ADD] links

--- src/assets/main.js
+++ src/assets/main.js
@@ -23,6 +23,7 @@ async function fetchData(urlApi) {
     let view = `
     ${videos.items.map(video => `
       <div class="group relative">
+        <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
@@ -32,6 +33,7 @@ async function fetchData(urlApi) {
           ${video.snippet.title}
           </h3>
         </div>
+        </a>
       </div>
     `).slice(0, 4).join('')}
     `;
